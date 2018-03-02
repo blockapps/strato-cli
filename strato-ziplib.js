becfa0b28aefd82c1af2e3543de0433c1ad1eb17
Tim Holland <tim@blockapps.net>
2018-03-02 17:37:56 -0500

    Use zip-dir instead of adm-zip

--- strato-ziplib.js
+++ strato-ziplib.js
@@ -1,8 +1,5 @@
-const fs = require('fs');
-const AdmZip = require('adm-zip');
-const path = require("path");
-const process = require("process");
-const walk = require("walk");
+const zipdir = require('zip-dir');
+
 /**
  * Create .zip file inside User's home directory /strato
  * @params dir String the directory to be zipped up in full.
@@ -12,17 +9,10 @@ const walk = require("walk");
 
 function zipFolder(dir, target) {
   return new Promise((resolve, reject) => {
-      const walker = walk.walk(path.resolve(dir));
-      const zip = new AdmZip();
-      const cwd = process.cwd();
-      walker.on("file", function(root, fileStats, next) {
-        const absPath = path.join(root, fileStats.name);
-        zip.addLocalFile(absPath, path.relative(dir, absPath));
-        next();
-      });
-      walker.on('errors', reject);
-      walker.on('end', async () => {
-        zip.writeZip(target);
+      zipdir(dir, {saveTo: target}, function (err, buffer) {
+        if (err) {
+          reject(err);
+        }
         resolve();
       });
   });
